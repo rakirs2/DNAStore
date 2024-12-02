e3332fe3d2899cbda70ebcbbb6cd1fb1fb04857f
Srikar Mylavarapu <srikarmylavarapu@gmail.com>
2024-12-01 18:27:47 -0600

    adding a quick parser

--- Program.cs
+++ Program.cs
@@ -2,13 +2,17 @@
 Console.WriteLine("Hello, World!");
 var file = File.Open("/Users/rakirs/github/DNAStore/data/ncbi_dataset/ncbi_dataset/data/GCA_009914755.4/GCA_009914755.4_T2T-CHM13v2.0_genomic.fna", FileMode.Open);
 
-byte[] byteArray = new byte[16];
+byte[] byteArray = new byte[1024];
 
 Console.WriteLine(file.Length);
 
 var bytesRead = 0;
 while (bytesRead < file.Length)
 {
-    bytesRead += file.Read(byteArray, 0, 16);
-    Console.WriteLine(System.Text.Encoding.UTF8.GetString(byteArray));
+    bytesRead += file.Read(byteArray, 0, 1024);
+    var bufferString = System.Text.Encoding.UTF8.GetString(byteArray);
+    if (bufferString.Contains('>'))
+    {
+        Console.WriteLine(bufferString);
+    }
 }
