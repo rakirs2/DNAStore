bb468f4eea6671dea7c72a8df1d41cd789aabff8
Srikar Mylavarapu <srmylavarapu@microsoft.com>
2025-02-09 08:19:40 -0600

    initial Tree Stuff

---
+++ Base/ITree.cs
@@ -0,0 +1,13 @@
+﻿namespace Base
+{
+    public interface ITree <T>
+    {
+        int Height { get; }
+
+        void Insert(T type);
+
+        void Remove(T type);
+
+        bool Contains(T type);
+    }
+}
