200bf1fa5c6c305d3c4b8834256cdd1a11c65985
Srikar Mylavarapu <srmylavarapu@microsoft.com>
2025-04-30 22:39:49 -0500

    updating

--- ConsoleRunner/InputProcessor.cs
+++ ConsoleRunner/InputProcessor.cs
@@ -9,7 +9,6 @@
             // TODO: reconsider style on this
             switch (request)
             {
-
                 case "analyzeString":
                     output = new SequenceAnalysis();
                     break;
@@ -22,6 +21,8 @@
                     // probably safe to do it this way
                     output = new SequenceAnalysis();
                     break;
+
+                // TODO: clean up the exit pathway
 
             }
             return output;
