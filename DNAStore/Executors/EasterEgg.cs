ecdadb9a0e019f3a6bde30bb38a80c80ed074511
Srikar Mylavarapu <srmylavarapu@microsoft.com>
2025-05-09 11:02:56 -0500

    fixing

--- DNAStore/Executors/EasterEgg.cs
+++ DNAStore/Executors/EasterEgg.cs
@@ -22,9 +22,7 @@ public class EasterEgg : BaseExecutor
         theWhy.Append("Welcome to DNA Store, a C# based implementation of everything bioinformatics related. ");
         theWhy.Append(
             "Today is 4/30/2025. At some point of analyzing file streams, I realized I want to focus on the life stream in my spare time. ");
-        theWhy.Append(
-            "Perhaps this is the real test to see how committed I am to a subject when I get to pick the subject matter. ");
-        theWhy.Append("I really do miss Biology and Chemistry. They were my first loves for a reason. This is my way of looking back");
+        theWhy.Append("I really do miss Biology and Chemistry. They were my first loves for a reason.");
 
         Console.WriteLine(theWhy.ToString());
     }
