2c739a06e213f92470c89f7162af1280d2a6cd35
Srikar Mylavarapu <SrikarMylavarapu@gmail.com>
2025-05-11 12:17:52 -0500

    wadding kmer simple solution

---
+++ Bio/Analysis/Interfaces/IKmerCounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bio.Analysis.Types
+{
+    public interface  IKmerCounter
+    {
+        string HighestFrequencyKmer { get; }
+        int KmerLength { get; }
+
+    }
+}
---
+++ BioTests/Analysis/Types/KmerCounterTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bio.Sequence.Types;
+
+namespace Bio.Analysis.Types.Tests
+{
+    [TestClass]
+    public class KmerCounterTests
+    {
+        // TODO: what does eee get counted 2 or 3x -- > is there a DNA related
+        // example here?
+        private AnySequence sequence = new AnySequence("aabbccddeeffeeggee");
+        [TestMethod]
+        public void KmerCounterTest()
+        {
+            var kmerCounter = new KmerCounter(sequence, 2);
+            Assert.AreEqual(2, kmerCounter.KmerLength);
+            Assert.AreEqual("ee", kmerCounter.HighestFrequencyKmer);
+            Assert.AreEqual(3, kmerCounter.CurrentHighestFrequency);
+        }
+    }
+}
