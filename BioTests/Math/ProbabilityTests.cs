ee935241e2aa33b7653f73c61e1b848acd205bb6
Srikar <srikarmylavarapu@gmail.com>
2025-05-13 07:33:22 -0500

    Expectedchildren (#57)
    * overlap graphs
    
    * working

--- BioTests/Math/ProbabilityTests.cs
+++ BioTests/Math/ProbabilityTests.cs
@@ -1,4 +1,5 @@
-﻿using Bio.Math;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bio.Math;
 using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
 
 namespace BioTests.Math;
@@ -17,5 +18,12 @@ public class ProbabilityTests
     {
         var allPerms = Probability.GetPermutations(Enumerable.Range(1, 3), 3);
         Assert.AreEqual(6, allPerms.Count());
+    }
+
+    [TestMethod()]
+    public void ExpectedDominantOffspringTest()
+    {
+        var total = Probability.ExpectedDominantOffspring(1, 0, 0, 1,0,1,2);
+        Assert.AreEqual(3.5, total);
     }
 }
