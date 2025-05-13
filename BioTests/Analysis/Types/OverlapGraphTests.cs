f658ca9d3c33d24d421b21c08530c301127140d7
Srikar Mylavarapu <SrikarMylavarapu@gmail.com>
2025-05-12 21:53:43 -0500

    overlap graphs

---
+++ BioTests/Analysis/Types/OverlapGraphTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bio.Analysis.Types;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bio.IO;
+
+namespace Bio.Analysis.Types.Tests
+{
+    [TestClass()]
+    public class OverlapGraphTests
+    {
+        private readonly string _filePath = Path.Combine(Directory.GetCurrentDirectory(),
+            "../../../../BioTests/TestData/OverlapFastas.fasta");
+
+        [TestMethod()]
+        public void OverlapGraphTest()
+        {
+            var result = new OverlapGraph(FastaParser.Read(_filePath), 3);
+            Assert.AreEqual(3, result.GetOverlaps().Count());
+        }
+    }
+}
---
+++ DNAStore/Executors/OverlapGraphExecutor.cs
@@ -0,0 +1,36 @@
+﻿using Bio.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bio.Analysis.Types;
+
+namespace DNAStore.Executors
+{
+    internal class OverlapGraphExecutor : BaseExecutor
+    {
+        protected override void GetInputs()
+        {
+            Console.WriteLine("Please input path to file");
+            var location = Console.ReadLine();
+            if (location != null) _fastas = FastaParser.Read(location);
+        }
+
+        protected override void CalculateResult()
+        {
+            _overlapGraph = new OverlapGraph(_fastas, 3);
+        }
+
+        protected override void OutputResult()
+        {
+            foreach (var tuple in _overlapGraph.GetOverlaps())
+            {
+                Console.WriteLine(tuple.Item1.Name + " " + tuple.Item2.Name);
+            }
+        }
+
+        private IList<Fasta>? _fastas;
+        private OverlapGraph _overlapGraph;
+    }
+}
