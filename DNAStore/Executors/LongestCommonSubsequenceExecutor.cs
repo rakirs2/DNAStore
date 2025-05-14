9c47539f79dbdcaf1e4d9da32a63118f7ce5ef56
Srikar <srikarmylavarapu@gmail.com>
2025-05-14 12:09:44 -0500

    Longestcommonsubsequence (#58)
    * overlap graphs
    
    * working
    
    * inital

---
+++ DNAStore/Executors/LongestCommonSubsequenceExecutor.cs
@@ -0,0 +1,34 @@
+﻿using Bio.Analysis.Types;
+using Bio.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bio.Sequence.Types;
+
+namespace DNAStore.Executors
+{
+    internal class LongestCommonSubsequenceExecutor : BaseExecutor
+    {
+        protected override void GetInputs()
+        {
+            Console.WriteLine("Please input path to file");
+            var location = Console.ReadLine();
+            if (location != null) _fastas = FastaParser.Read(location);
+        }
+
+        protected override void CalculateResult()
+        {
+            _result = new LongestCommonSubsequence(_fastas);
+        }
+
+        protected override void OutputResult()
+        {
+            Console.WriteLine($"A longest common subsequence is: \n{_result.GetAnyLongest().RawSequence}");
+        }
+
+        private List<Fasta> _fastas;
+        private LongestCommonSubsequence _result;
+    }
+}
