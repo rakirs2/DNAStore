48402082f0075bf0af1ccbdee78b7fe1c9da4997
Srikar Mylavarapu <SrikarMylavarapu@gmail.com>
2025-05-18 13:18:57 -0500

    known motif tests

---
+++ BioTests/Analysis/Types/KnownMotifsTests.cs
@@ -0,0 +1,12 @@
+﻿using Bio.Analysis.Types;
+
+namespace BioTests.Analysis.Types;
+public class KnownMotifsTests
+{
+    [TestMethod]
+    public void NGlycostatin()
+    {
+        Assert.IsTrue(KnownMotifs.NGlycostatin.IsMatch("NNSN"));
+        Assert.IsFalse(KnownMotifs.NGlycostatin.IsMatchStrict("NNSNA"));
+    }
+}
