f8d5e13cbf62314b04eee5f1ad6b77b597fd8309
Srikar Mylavarapu <SrikarMylavarapu@gmail.com>
2025-05-21 17:59:48 -0500

    expanding motiffinder

--- DNAStore/Executors/MotifFinder.cs
+++ DNAStore/Executors/MotifFinder.cs
@@ -9,20 +9,29 @@ public class MotifFinder : BaseExecutor
     {
         Console.WriteLine("Please enter the first sequence");
         a = new AnySequence(Console.ReadLine());
-        Console.WriteLine("Please enter the second sequence");
-        b = new Motif(Console.ReadLine());
+        Console.WriteLine("Please enter the motif");
+        var motifString = Console.ReadLine();
+        Console.WriteLine("Please enter the expected Length");
+        var expectedLength = int.Parse(Console.ReadLine());
+        b = new Motif(motifString, expectedLength);
+
+        Console.WriteLine("Is Zero Index 'y'");
+        var input = Console.ReadLine();
+        _isZeroIndex = input.Equals("y", StringComparison.OrdinalIgnoreCase);
     }
 
     protected override void CalculateResult()
     {
-        result = a.MotifLocations(b);
+        result = a.MotifLocations(b, _isZeroIndex);
     }
 
     protected override void OutputResult()
     {
-        Console.WriteLine($"The One Index Locations are: \n{string.Join(" ", result)}");
+        var indexType = _isZeroIndex ? "Zero" : "One";
+        Console.WriteLine($"The {indexType}-Index Locations are: \n{string.Join(" ", result)}");
     }
 
+    private bool _isZeroIndex;
     private AnySequence? a;
     private Motif? b;
     private long[]? result;
