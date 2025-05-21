05f6b9534939de2f80ff5eba7d9123e2c8737249
Srikar Mylavarapu <SrikarMylavarapu@gmail.com>
2025-05-21 10:34:32 -0500

    updated

---
+++ Base/Utils/StringUtils.cs
@@ -0,0 +1,22 @@
+﻿namespace Base.Utils;
+public static class StringUtils
+{
+    /// <summary>
+    /// Swaps the characters on the reference passed in
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="indexA"></param>
+    /// <param name="indexB"></param>
+    public static string SwapIndex(string input, int indexA, int indexB)
+    {
+        if (input == null || indexA > input.Length - 1 || indexB > input.Length - 1 || indexA < 0 || indexB < 0)
+        {
+            throw new InvalidDataException("Something wrong wiht the inputs");
+        }
+
+        // Swaps characters in a string.
+        char[] array = input.ToCharArray();
+        (array[indexA], array[indexB]) = (array[indexB], array[indexA]);
+        return new string(array);
+    }
+}
---
+++ BaseTests/Utils/StringUtilsTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Base.Utils.Tests;
+
+[TestClass()]
+public class StringUtilsTests
+{
+    [TestMethod()]
+    public void SwapIndexTest()
+    {
+        var output = StringUtils.SwapIndex("test", 0, 1);
+        Assert.IsTrue(output.Equals("etst"));
+    }
+
+    [TestMethod()]
+    [ExpectedException(typeof(InvalidDataException))]
+    public void SwapIndexTestOutOfBoundsNegative()
+    {
+        var output = StringUtils.SwapIndex("test", -1, 1);
+    }
+
+    [TestMethod()]
+    [ExpectedException(typeof(InvalidDataException))]
+    public void SwapIndexTestOutOfBoundsGreaterThanLength()
+    {
+        var output = StringUtils.SwapIndex("test", 5, 1);
+    }
+
+    [TestMethod()]
+    [ExpectedException(typeof(InvalidDataException))]
+    public void SwapIndexTestOutOfBoundsNullString()
+    {
+        var output = StringUtils.SwapIndex(null, 5, 1);
+    }
+}
