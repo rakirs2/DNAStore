bb77d2b3fbca7ce06493728a674a9b9872c9330d
Srikar <srikarmylavarapu@gmail.com>
2025-05-24 09:28:49 -0500

    Window kmers (#68)
    * initial
    
    * working initially
    
    * working clump finder

---
+++ Bio/Analysis/Types/KmerClumpCounter.cs
@@ -0,0 +1,49 @@
+﻿using Bio.Analysis.Interfaces;
+using Bio.Sequence.Types;
+
+namespace Bio.Analysis.Types;
+public class KmerClumpCounter : IKmerClumpCounter
+{
+    // TODO: there's probably some work to generalize this later
+    // TODO: Design phil -- do I really care if there's calculations going on construction?
+    // TODO: Get a separate calculate step for all of these data structures
+    public KmerClumpCounter(AnySequence sequence, int scanLength, int kmerLength, int minCount)
+    {
+        Sequence = sequence;
+        ScanLength = scanLength;
+        KmerLength = kmerLength;
+        MinCount = minCount;
+        // Always assume that we are adding to a proper Dictionary
+        for (int i = 0; i <= sequence.RawSequence.Length - KmerLength; i++)
+        {
+            while (_orderQueue.Count >= ScanLength)
+            {
+                var temp = _orderQueue.Dequeue();
+                _slidingCounter[temp] -= 1;
+            }
+
+            var current = sequence.RawSequence.Substring(i, kmerLength);
+            _orderQueue.Enqueue(current);
+
+            _slidingCounter.TryAdd(current, 0);
+            _slidingCounter[current] += 1;
+            if (_slidingCounter[current] >= MinCount)
+            {
+                ValidKmers.Add(current);
+            }
+
+        }
+    }
+
+    public int ScanLength { get; }
+    public int KmerLength { get; }
+    public AnySequence Sequence { get; }
+    public int MinCount { get; }
+
+    public HashSet<string> ValidKmers { get; } = new HashSet<string>();
+
+
+    private Dictionary<string, int> _slidingCounter = new();
+
+    private Queue<string> _orderQueue = new();
+}
---
+++ BioTests/Analysis/Types/KmerClumpCounterTests.cs
@@ -0,0 +1,39 @@
+﻿using Bio.Sequence.Types;
+
+namespace Bio.Analysis.Types.Tests;
+
+[TestClass()]
+public class KmerClumpCounterTests
+{
+    [TestMethod()]
+    public void KmerClumpCounterTest()
+    {
+        var seq = new AnySequence(
+            "CGGACTCGACAGATGTGAAGAAATGTGAAGACTGAGTGAAGAGAAGAGGAAACACGACACGACATTGCGACATAATGTACGAATGTAATGTGCCTATGGC");
+        var counter = new KmerClumpCounter(seq, 20, 5, 3);
+        Assert.AreEqual(20, counter.ScanLength);
+        Assert.AreEqual(5, counter.KmerLength);
+        Assert.AreEqual(3, counter.MinCount);
+    }
+
+    [TestMethod()]
+    public void GenericCase()
+    {
+        var seq = new AnySequence(
+            "CGGACTCGACAGATGTGAAGAAATGTGAAGACTGAGTGAAGAGAAGAGGAAACACGACACGACATTGCGACATAATGTACGAATGTAATGTGCCTATGGC");
+        var counter = new KmerClumpCounter(seq, 75, 5, 4);
+        var expected = new HashSet<string> { "CGACA", "GAAGA", "AATGT" };
+        Assert.IsTrue(counter.ValidKmers.SetEquals(expected));
+    }
+
+    [TestMethod]
+    // From book test cases
+    public void OffBy1()
+    {
+        var seq = new AnySequence(
+            "ACGTACGT");
+        var counter = new KmerClumpCounter(seq, 5, 1, 2);
+        var expected = new HashSet<string> { "T", "C", "G", "A" };
+        Assert.IsTrue(counter.ValidKmers.SetEquals(expected));
+    }
+}
