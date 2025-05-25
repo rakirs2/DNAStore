de05a199e66e991775cd403e914508e30a4aec86
Srikar <srikarmylavarapu@gmail.com>
2025-05-25 08:39:02 -0500

    Fibonacci (#70)
    * initial
    
    * nits

---
+++ Base/Algorithms/Fibonacci.cs
@@ -0,0 +1,27 @@
+﻿namespace Base.Algorithms;
+public static class Fibonacci
+{
+    public static int GetNth(int i)
+    {
+        if (i < 1)
+            throw new InvalidDataException();
+
+        if (i == 1)
+        {
+            return 1;
+        }
+
+        if (i == 2)
+        {
+            return 1;
+        }
+        var tracker = new int[i];
+        tracker[0] = 1;
+        tracker[1] = 1;
+        for (int j = 2; j < i; j++)
+        {
+            tracker[j] = tracker[j - 1] + tracker[j - 2];
+        }
+        return tracker.Last();
+    }
+}
