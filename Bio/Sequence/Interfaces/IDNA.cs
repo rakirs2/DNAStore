68807118ca13e9efba0679e9a7b78f96b77a532b
Srikar Mylavarapu <SrikarMylavarapu@gmail.com>
2025-05-25 16:07:15 -0500

    adding IDNA

---
+++ Bio/Sequence/Interfaces/IDNA.cs
@@ -0,0 +1,9 @@
+﻿namespace Bio.Sequence.Interfaces;
+public interface IDNA
+{
+    /// <summary>
+    ///  Returns the list of all possible restriction sites and their lengths
+    /// </summary>
+    /// <returns></returns>
+    List<Tuple<int, int>> CalculateRestrictionSites();
+}
