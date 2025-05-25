e59915f0dadb81f0c3524166d9ae77c7a2a369f2
Srikar Mylavarapu <SrikarMylavarapu@gmail.com>
2025-05-25 16:54:48 -0500

    updating

---
+++ DNAStore/Executors/RestictionSites.cs
@@ -0,0 +1,28 @@
+﻿using Bio.Sequence.Types;
+
+namespace DNAStore.Executors;
+internal class RestictionSites : BaseExecutor
+{
+
+    protected override void GetInputs()
+    {
+        Console.WriteLine("Enter Sequence To be analyzed");
+        sequence = new DNASequence(Console.ReadLine());
+    }
+
+    protected override void CalculateResult()
+    {
+        output = sequence.RestrictionSites();
+    }
+
+    protected override void OutputResult()
+    {
+        foreach (var tuple in output)
+        {
+            Console.WriteLine($"{tuple.Item1} {tuple.Item2.}");
+        }
+    }
+
+    private DNASequence? sequence;
+    private List<Tuple<int, int>>? output;
+}
