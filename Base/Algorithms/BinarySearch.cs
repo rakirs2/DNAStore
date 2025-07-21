3c60da66b9590bae9c9540818973ab53dd079d0b
Srikar Mylavarapu <SrikarMylavarapu@gmail.com>
2025-07-21 17:57:04 -0500

    simple

---
+++ Base/Algorithms/BinarySearch.cs
@@ -0,0 +1,54 @@
+﻿namespace Base.Algorithms;
+public static class BinarySearch
+{
+    // TODO: generalize this, C# already has it
+    // A fun exercise would be to use this for 
+    // This assumes the array is already sorted.
+    // TODO rename to "custom" something
+    public static bool Contains(List<int> arrayToCheck, int value)
+    {
+        var right = arrayToCheck.Count - 1;
+        var left = 0;
+        while (left < right)
+        {
+            int middle = left + ((right - left) / 2);
+            if (arrayToCheck[middle] == value)
+                return true;
+            if (arrayToCheck[middle] < value)
+                left = middle + 1;
+            if (arrayToCheck[middle] > value)
+                right = middle - 1;
+        }
+
+        return false;
+    }
+
+    public static int GetIndexAt(List<int> arrayToCheck, int value)
+    {
+        var right = arrayToCheck.Count - 1;
+        var left = 0;
+        while (left <= right)
+        {
+            int middle = left + ((right - left) / 2);
+            if (arrayToCheck[middle] == value)
+                return middle;
+            else if (arrayToCheck[middle] < value)
+                left = middle + 1;
+            else
+                right = middle - 1;
+        }
+
+        return -1;
+    }
+
+    public static List<int> GetIndices(List<int> arrayToCheck, List<int> values)
+    {
+        var output = new List<int>();
+        foreach (var value in values)
+        {
+            output.Add(GetIndexAt(arrayToCheck, value));
+        }
+
+        return output;
+    }
+}
---
+++ BaseTests/Algorithms/BinarySearchTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Base.Algorithms.Tests;
+
+[TestClass()]
+public class BinarySearchTests
+{
+    private List<int> _array = new() { 1, 2, 3, 4, 5 };
+    [TestMethod()]
+    public void ContainsValueTrue()
+    {
+        Assert.IsTrue(BinarySearch.Contains(_array, 1));
+    }
+
+    [TestMethod()]
+    public void ContainsValueFalse()
+    {
+        Assert.IsFalse(BinarySearch.Contains(_array, 6));
+    }
+
+    [TestMethod()]
+    public void ContainsIndex()
+    {
+        Assert.AreEqual(0, BinarySearch.GetIndexAt(_array, 1));
+        Assert.AreEqual(3, BinarySearch.GetIndexAt(_array, 4));
+        Assert.AreEqual(-1, BinarySearch.GetIndexAt(_array, 6));
+        Assert.AreEqual(-1, BinarySearch.GetIndexAt(_array, 0));
+    }
+
+    [TestMethod()]
+    public void GetIndicesTest()
+    {
+        Assert.IsTrue(Enumerable.SequenceEqual(new List<int> { 1, 2, 3 }, BinarySearch.GetIndices(_array, new List<int>() { 2, 3, 4 })));
+    }
+}
