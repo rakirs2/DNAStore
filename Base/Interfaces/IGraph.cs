7eb928de66939e843f9cbaa7fff8bc172e0a3daf
Srikar Mylavarapu <SrikarMylavarapu@gmail.com>
2025-08-09 14:13:23 -0500

    adding interface

---
+++ Base/Interfaces/IGraph.cs
@@ -0,0 +1,10 @@
+﻿namespace Base.Interfaces;
+internal interface IGraph<T>
+{
+    void Insert(T start, T end);
+    void Remove(T item);
+    Dictionary<T, HashSet<T>> GetEdgeList();
+    int EdgesToMakeTree();
+    int NumNodes { get; }
+    int NumEdges { get; }
+}
