735e5012d2e2ac7ddabb2404426b86c24d2211c3
Srikar <srikarmylavarapu@gmail.com>
2025-11-08 15:55:17 -0600

    adding matrix as array

--- Base/DataStructures/AlignmentMatrix.cs
+++ Base/DataStructures/AlignmentMatrix.cs
@@ -9,41 +9,17 @@ public class AlignmentMatrix : IAlignmentMatrix
     private List<List<Node>> _matrix;
     public AlignmentMatrix(string a, string b)
     {
-        var _matrix = new List<List<Node>>();
-        for (int i = 0; i < a.Length +1; i++)
-        {
-            _matrix.Add(new List<Node>(b.Length +1));
-        }
-
-        // force edges to be None and 0.
-        foreach (var item in _matrix)
-        {
-            item.Add(new Node());
-        }
+        Node[,] _matrix = new Node[a.Length +1,b.Length+1];
+        for (int i=0;i<(a.Length+1)*(b.Length+1);i++) _matrix[i%(a.Length+1),i/(a.Length+1)]=new Node(); 
         
-        for(int i = 0; i < b.Length + 1; i++)
-        {
-            if (i == 0)
-            {
-                continue;
-            }
-            _matrix[0].Add( new Node());
-        }
 
         for (int i = 0; i < a.Length; i++)
         {
             for (int j = 0; j < b.Length; j++)
             {
                 // what value do we add?
-                _matrix[i].Add(new Node( ));
-                Console.WriteLine($"{_matrix[i]}, {_matrix[j]}");
+                Console.WriteLine($"{_matrix[i,j].Value}");
             }
-        }
-
-        Debug.Assert(_matrix.Count == a.Length + 1);
-        foreach (var item in _matrix)
-        {
-            Debug.Assert(item.Count == b.Length + 1);
         }
     }
 
