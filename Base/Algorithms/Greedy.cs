8e3485ba9f3e1b85c1cc17432776f81428de7abc
Srikar <srikarmylavarapu@gmail.com>
2025-11-13 10:08:24 -0600

    updating

---
+++ Base/Algorithms/Greedy.cs
@@ -0,0 +1,23 @@
+namespace BaseTests.Algorithms;
+
+public class Greedy
+{
+    public static int LeastCoins(int[] coinValues, int target)
+    {
+        var coinTracker = new int[target + 1];
+        Array.Fill(coinTracker, target + 1);
+        coinTracker[0] = 0;
+        for (int i = 1; i <= target; i++)
+        {
+            foreach (var coin in coinValues)
+            {
+                if (coin <= i)
+                {
+                    coinTracker[i] = Math.Min(coinTracker[i], coinTracker[i - coin] + 1);
+                }
+            }
+        }
+        
+        return  coinTracker[target] > target ? -1 : coinTracker[target];
+    }
+}
