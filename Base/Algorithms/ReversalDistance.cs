9f7c9e82694df90107e93815bd6fa14d337358cd
Srikar <srikarmylavarapu@gmail.com>
2025-12-15 14:39:48 -0600

    initally working, needs to add breakpoints

---
+++ Base/Algorithms/ReversalDistance.cs
@@ -0,0 +1,75 @@
+using Base.Utils;
+
+namespace Base.Algorithms;
+
+public class ReversalDistance
+{
+    private int[] _a;
+    private int[] _b;
+    private ReversalDistance(int[] a, int[] b)
+    {
+        if (a.Length != b.Length)
+        {
+            throw new ArgumentException("Lengths must be equal");
+        } 
+        
+        _a = a;
+        _b = b;
+    }
+    
+    /// <summary>
+    /// There are n^2 possible reversals at every iteration in this implementation.
+    /// TODO: Hannenhali and Pevzner's clearer alg (hopefully?)
+    /// </summary>
+    /// <returns></returns>
+    private int Calculate()
+    {
+        Queue<int[]> currentIteration = new Queue<int[]>();
+        Queue<int[]> nextIteration = new Queue<int[]>();
+
+        HashSet<int[]> traversed = new HashSet<int[]>( new IntArrayComparer());
+        currentIteration.Enqueue(_a);
+        var currentDepth = 0;
+        
+        while (currentIteration.Count != 0 || nextIteration.Count != 0 && currentDepth <= _a.Length)
+        {
+            var temp = currentIteration.Dequeue();
+            if (temp.SequenceEqual(_b))
+            {
+                return currentDepth;
+            }
+            
+            traversed.Add(temp);
+
+            for (int i = 1; i <= temp.Length; i++)
+            {
+                for (int j = 0; j + i <= _b.Length; j++)
+                {
+
+                    var other = (int[])temp.Clone();
+                    Array.Reverse(other, j, i);
+
+                    if (!traversed.Contains(other))
+                    {
+                        nextIteration.Enqueue(other);
+                    }
+                }
+            }
+            
+            if (currentIteration.Count == 0)
+            {
+                currentIteration = nextIteration;
+                nextIteration = new Queue<int[]>();
+                currentDepth ++;
+            }
+        }
+        
+        // This shouldn't be reached
+        return -1;
+    }
+
+    public static int Calculate(int[] a, int[] b)
+    {
+        return new ReversalDistance(a, b).Calculate();
+    }
+}
---
+++ BaseTests/Algorithms/ReversalDistanceTest.cs
@@ -0,0 +1,89 @@
+using Base.Algorithms;
+
+namespace BaseTests.Algorithms;
+
+[TestClass]
+public class ReversalDistanceTest
+{
+
+    [TestMethod]
+    public void ReversalDistanceInvalidDistances()
+    {
+        var a = new int[] { 1,2,3};
+        var b = new int[] { 0 };
+        Assert.ThrowsExactly<ArgumentException>(()=>ReversalDistance.Calculate(a,b));
+    }
+    
+    [TestMethod]
+    public void SimpleEquality()
+    {
+        var a = new int[] { 1};
+        var b = new int[] { 1 };
+        Assert.AreEqual(0, ReversalDistance.Calculate(a,b));
+    }
+    
+    [TestMethod]
+    public void SingleReversal()
+    {
+        var a = new int[] { 1,2};
+        var b = new int[] { 2,1};
+        Assert.AreEqual(1, ReversalDistance.Calculate(a,b));
+    }
+    
+    [TestMethod]
+    public void Given()
+    {
+        var a = new int[] { 1, 2, 3 ,4 ,5};
+        var b = new int[] { 3, 1, 5, 2, 4};
+        Assert.AreEqual(4, ReversalDistance.Calculate(a,b));
+    }
+    [TestMethod]
+    public void Size6()
+    {
+        var a = new int[] { 1, 2, 3 ,4,6 ,5};
+        var b = new int[] { 6, 3, 1, 5, 2, 4};
+        Assert.AreEqual(4, ReversalDistance.Calculate(a,b));
+    }
+    [TestMethod]
+    public void Size7()
+    {
+        var a = new int[] { 1, 2,7, 3 ,4,6 ,5};
+        var b = new int[] { 6, 3, 1, 5, 2, 4,7};
+        Assert.AreEqual(4, ReversalDistance.Calculate(a,b));
+    }
+    
+    [TestMethod]
+    public void Size8()
+    {
+        var a = new int[] { 6,1, 7,8, 2, 3 ,4 ,5};
+        var b = new int[] { 1,2,3,4,5,6,7,8};
+        Assert.AreEqual(3, ReversalDistance.Calculate(a,b));
+    }
+    
+   
+    [TestMethod]
+    [Ignore]
+    public void GivenTests()
+    {
+        var a = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        var b = new int[] { 3, 1, 5, 2, 7, 4, 9, 6, 10, 8 };
+        Assert.AreEqual(9, ReversalDistance.Calculate(a,b));
+
+        var c = new int[] { 3, 10, 8, 2, 5, 4, 7, 1, 6, 9 };
+        var d = new int[] { 5, 2, 3, 1, 7, 4, 10, 8, 6, 9 };
+        Assert.AreEqual(6, ReversalDistance.Calculate(c,d));
+
+        var e = new int[] { 8, 6, 7, 9, 4, 1, 3, 10, 2, 5 };
+        var f = new int[] { 8, 2, 7, 6, 9, 1, 5, 3, 10, 4 };
+        Assert.AreEqual(5, ReversalDistance.Calculate(e,f));
+
+        var g = new int[] { 3, 9, 10, 4, 1, 8, 6, 7, 5, 2 };
+        var h = new int[] { 2, 9, 8, 5, 1, 7, 3, 4, 6, 10 };
+        Assert.AreEqual(7, ReversalDistance.Calculate(g,h));
+
+        var i = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        var j = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Assert.AreEqual(0, ReversalDistance.Calculate(g,h));
+    }
+    
+}
