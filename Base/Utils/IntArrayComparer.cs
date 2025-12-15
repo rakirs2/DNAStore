187862b74398b6e7d589c6ac525f73e6e273f468
Srikar <srikarmylavarapu@gmail.com>
2025-12-15 17:32:24 -0600

    ReversalDistance p1 (#146)
    * initally working, needs to add breakpoints
    
    * adding breakpoints

---
+++ Base/Utils/IntArrayComparer.cs
@@ -0,0 +1,30 @@
+namespace Base.Utils;
+
+public class IntArrayComparer : IEqualityComparer<int[]>
+{
+    public bool Equals(int[] x, int[] y)
+    {
+        // Check for reference equality first for performance
+        if (ReferenceEquals(x, y)) return true;
+        // Check for nulls
+        if (x is null || y is null) return false;
+        // Check for sequence equality using LINQ
+        return x.SequenceEqual(y);
+    }
+
+    public int GetHashCode(int[] obj)
+    {
+        if (obj is null) return 0;
+        
+        // some prime number hash
+        unchecked 
+        {
+            int hash = 17;
+            foreach (var item in obj)
+            {
+                hash = hash * 23 + item.GetHashCode(); 
+            }
+            return hash;
+        }
+    }
+}
