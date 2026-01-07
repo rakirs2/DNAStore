8acce970a1f77086a39b1c8c993fc692fc54ce13
Srikar <srikarmylavarapu@gmail.com>
2026-01-07 09:54:53 -0600

    updating

---
+++ RosalindTestRunner/Program.cs
@@ -0,0 +1,12 @@
+﻿using RosalindTestRunner;
+
+var currentInput = "notExist";
+
+while (currentInput != "exit")
+{
+    Console.WriteLine("What would you like to do");
+    Console.WriteLine($"Current options are to: {string.Join(", ", InputProcessor.ExecutorRegistry.Map.Keys.ToArray())}");
+    currentInput = Console.ReadLine();
+    var executor = InputProcessor.GetExecutor(currentInput);
+    executor.Run();
+}
