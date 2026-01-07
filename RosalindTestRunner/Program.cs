318784ea97551b1350e4447f8ab7da6053c70f3b
Srikar <srikarmylavarapu@gmail.com>
2026-01-07 10:01:47 -0600

    Adding back test runner (#203)
    * updating
    
    * adding values, registry back

---
+++ RosalindTestRunner/Program.cs
@@ -0,0 +1,13 @@
+﻿using RosalindTestRunner;
+
+var currentInput = "notExist";
+
+while (currentInput != "exit")
+{
+    Console.WriteLine("What would you like to do");
+    // TODO: a good exercise -- fuzzy match these to best chance
+    Console.WriteLine($"Current options are to: {string.Join(", ", InputProcessor.ExecutorRegistry.GetExecutorNames())}");
+    currentInput = Console.ReadLine();
+    var executor = InputProcessor.GetExecutor(currentInput);
+    executor.Run();
+}
